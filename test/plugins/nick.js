5bcafaf5ac49ba0a5710ee1e88ec85f15846197c
TJ Holowaychuk <tj@vision-media.ca>
2013-10-18 10:41:46 -0700

    add "nick" event

--- test/plugins/nick.js
+++ test/plugins/nick.js
@@ -10,5 +10,17 @@ describe('nick()', function(){
       stream.write(':cameron.freenode.net 001 tobi :Welcome to the freenode Internet Relay Chat Network tobi\r\n');
       client.me.should.equal('tobi');
     })
+
+    it('should emit "nick"', function(done){
+      var stream = new Stream;
+      var client = irc(stream);
+      
+      client.on('nick', function(nick){
+        nick.should.equal('tobi');
+        done();
+      });
+
+      stream.write(':cameron.freenode.net 001 tobi :Welcome to the freenode Internet Relay Chat Network tobi\r\n');
+    })
   })
 })
