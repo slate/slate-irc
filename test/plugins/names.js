ace8745ab070625bded0dcfbe7b86088f620b0ff
Mattias Erming <mattias@mattiaserming.com>
2014-04-29 02:09:25 +0200

    fix user modes

--- test/plugins/names.js
+++ test/plugins/names.js
@@ -6,12 +6,11 @@ describe('names()', function(){
   describe('client.names(chan, fn)', function(){
     it('should respond with user names', function(done){
       var stream = new Stream;
-      
       var client = irc(stream);
 
       client.names('#luna-lang', function(err, names){
         if (err) return done(err);
-        names.should.eql(['foo', 'bar', 'baz', 'some', 'more']);
+        names.should.eql({foo: '', bar: '', baz: '', some: '', more: ''});
         done();
       });
 
@@ -22,35 +21,33 @@ describe('names()', function(){
       });
     })
   })
-
+  
   it('should emit "names"', function(done){
     var stream = new Stream;
-    
     var client = irc(stream);
-
+  
     client.on('names', function(e){
       e.channel.should.equal('#luna-lang');
-      e.names.should.eql('one two three foo bar baz'.split(' '));
+      e.names.should.eql({one: '', two: '', three: '', foo: '', bar: '', baz: ''});
       done();
     });
-
+  
     stream.write(':tobi!~tobi@184.151.231.170 JOIN #luna-lang\r\n');
     stream.write(':rothfuss.freenode.net 353 tjholowaychuk = #luna-lang :one two three\r\n');
     stream.write(':rothfuss.freenode.net 353 tjholowaychuk = #luna-lang :foo bar baz\r\n');
     stream.write(':rothfuss.freenode.net 366 tjholowaychuk #luna-lang :End of /NAMES list.\r\n');
   })
-
+  
   it('should strip @ / +', function(done){
     var stream = new Stream;
-    
     var client = irc(stream);
-
+  
     client.on('names', function(e){
       e.channel.should.equal('##luna-lang');
-      e.names.should.eql(['tjholowaychuk']);
+      e.names.should.eql({'tjholowaychuk': '@'});
       done();
     });
-
+  
     stream.write(':tobi!~tobi@184.151.231.170 JOIN #luna-lang\r\n');
     stream.write(':rothfuss.freenode.net 353 tjholowaychuk @ ##luna-lang :@tjholowaychuk\r\n');
     stream.write(':rothfuss.freenode.net 366 tjholowaychuk ##luna-lang :End of /NAMES list.\r\n');
