84bb7780f184d0b2cd6ae58a8af1085269162f94
Mattias Erming <mattias@mattiaserming.com>
2014-06-06 21:29:06 +0200

    retain user modes

--- test/plugins/names.js
+++ test/plugins/names.js
@@ -10,12 +10,18 @@ describe('names()', function(){
 
       client.names('#luna-lang', function(err, names){
         if (err) return done(err);
-        names.should.eql({foo: '', bar: '', baz: '', some: '', more: ''});
+        names.should.eql([
+          {name: 'foo', mode: '@'},
+          {name: 'bar', mode: '+'},
+          {name: 'baz', mode: ''},
+          {name: 'some', mode: ''},
+          {name: 'more', mode: ''}
+        ]);
         done();
       });
 
       setImmediate(function(){
-        stream.write(':pratchett.freenode.net 353 tjholowaychuk = #luna-lang :foo bar baz\r\n');
+        stream.write(':pratchett.freenode.net 353 tjholowaychuk = #luna-lang :@foo +bar baz\r\n');
         stream.write(':pratchett.freenode.net 353 tjholowaychuk = #luna-lang :some more\r\n');
         stream.write(':pratchett.freenode.net 366 tjholowaychuk #luna-lang :End of /NAMES list.\r\n');
       });
@@ -28,28 +34,38 @@ describe('names()', function(){
   
     client.on('names', function(e){
       e.channel.should.equal('#luna-lang');
-      e.names.should.eql({one: '', two: '', three: '', foo: '', bar: '', baz: ''});
+      e.names.should.eql([
+        {name: 'one', mode: ''},
+        {name: 'two', mode: ''},
+        {name: 'three', mode: ''},
+        {name: 'foo', mode: '@'},
+        {name: 'bar', mode: '@'},
+        {name: 'baz', mode: ''}
+      ]);
       done();
     });
   
     stream.write(':tobi!~tobi@184.151.231.170 JOIN #luna-lang\r\n');
     stream.write(':rothfuss.freenode.net 353 tjholowaychuk = #luna-lang :one two three\r\n');
-    stream.write(':rothfuss.freenode.net 353 tjholowaychuk = #luna-lang :foo bar baz\r\n');
+    stream.write(':rothfuss.freenode.net 353 tjholowaychuk = #luna-lang :@foo @bar baz\r\n');
     stream.write(':rothfuss.freenode.net 366 tjholowaychuk #luna-lang :End of /NAMES list.\r\n');
   })
   
-  it('should strip @ / +', function(done){
+  it('should retain @ / +', function(done){
     var stream = new Stream;
     var client = irc(stream);
   
     client.on('names', function(e){
       e.channel.should.equal('##luna-lang');
-      e.names.should.eql({'tjholowaychuk': '@'});
+      e.names.should.eql([
+        {name: 'tjholowaychuk', mode: '@'},
+        {name: 'tobi', mode: '+'}
+      ]);
       done();
     });
   
     stream.write(':tobi!~tobi@184.151.231.170 JOIN #luna-lang\r\n');
-    stream.write(':rothfuss.freenode.net 353 tjholowaychuk @ ##luna-lang :@tjholowaychuk\r\n');
+    stream.write(':rothfuss.freenode.net 353 tjholowaychuk @ ##luna-lang :@tjholowaychuk +tobi\r\n');
     stream.write(':rothfuss.freenode.net 366 tjholowaychuk ##luna-lang :End of /NAMES list.\r\n');
   })
 })
