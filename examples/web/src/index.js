1ee895af09179bee47341c3691cd20ba1fbfad1d
Hyeon Kim <simnalamburt@gmail.com>
2025-03-06 16:00:10 +0900

    examples/web: `biome format`

--- examples/web/src/index.js
+++ examples/web/src/index.js
@@ -3,12 +3,14 @@ import websocket from 'websocket-stream'
 import BrowserStdout from 'browser-stdout'
 
 const stream = new websocket('ws://localhost', 'binary')
-stream.on('error', error => { throw error })
+stream.on('error', (error) => {
+  throw error
+})
 
 const client = irc(stream)
-client.use(irc => irc.stream.pipe(BrowserStdout())) // Pipe to browser log.
+client.use((irc) => irc.stream.pipe(BrowserStdout())) // Pipe to browser log.
 client.pass('pass')
-client.nick(`slate-${Math.random() * 100000 | 0}`)
+client.nick(`slate-${(Math.random() * 100000) | 0}`)
 client.user('tobi', 'Tobi Ferret')
 
 const root = document.getElementById('root')
