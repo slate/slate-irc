3eae119812fe85835aab2573e6ca41a993bc36dd
Hyeon Kim <simnalamburt@gmail.com>
2025-03-06 15:52:00 +0900

    examples/web: Replace browserify with rsbuild

--- examples/web/src/index.js
+++ examples/web/src/index.js
@@ -1,6 +1,6 @@
-const irc = require('slate-irc')
-const websocket = require('websocket-stream')
-const BrowserStdout = require('browser-stdout')
+import irc from 'slate-irc'
+import websocket from 'websocket-stream'
+import BrowserStdout from 'browser-stdout'
 
 const stream = new websocket('ws://localhost', 'binary')
 stream.on('error', error => { throw error })
@@ -10,3 +10,6 @@ client.use(irc => irc.stream.pipe(BrowserStdout())) // Pipe to browser log.
 client.pass('pass')
 client.nick(`slate-${Math.random() * 100000 | 0}`)
 client.user('tobi', 'Tobi Ferret')
+
+const root = document.getElementById('root')
+root.textContent = 'Open your console and see the irc log!'
