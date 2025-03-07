a9d2c5b6a8edd451c83781702a69c3c2997ae0dd
Hyeon Kim <simnalamburt@gmail.com>
2025-03-07 21:15:15 +0900

    websockify: Simplify codes, remove optimist dependency

--- examples/web/websockify/websockify.js
+++ examples/web/websockify/websockify.js
@@ -1,25 +1,40 @@
 #!/usr/bin/env node
-import net from 'node:net'
+import { createConnection } from 'node:net'
+import { argv } from 'node:process'
 
-import optimist from 'optimist'
 import { WebSocketServer } from 'ws'
 
-const argv = optimist.argv
+function show_usage_then_exit() {
+  console.error('websockify.js source_port target_addr target_port')
+  process.exit(1)
+}
 
-// Handle new WebSocket client
-function new_client(client, req) {
-  const clientAddr = client._socket.remoteAddress
+if (argv.length !== 5) {
+  show_usage_then_exit()
+}
+const source_port = Number(argv[2])
+const target_host = argv[3]
+const target_port = Number(argv[4])
+if (Number.isNaN(source_port) || Number.isNaN(target_port)) {
+  show_usage_then_exit()
+}
 
+console.log(
+  `Proxying ws://localhost:${source_port}/ to tcp://${target_host}:${target_port}`,
+)
+
+const wss = new WebSocketServer({ port: source_port })
+
+wss.on('connection', (client, req) => {
   console.log(req.url)
-  const log = (msg) => {
-    console.log(` ${clientAddr}: ${msg}`)
-  }
+
+  const log = (msg) => console.log(`${client._socket.remoteAddress}:\t${msg}`)
   log('WebSocket connection')
   log(`Version ${client.protocolVersion}, subprotocol: ${client.protocol}`)
 
-  const target = net.createConnection(target_port, target_host, () => {
-    log('connected to target')
-  })
+  const target = createConnection(target_port, target_host, () =>
+    log('connected to target'),
+  )
   target.on('data', (data) => {
     try {
       client.send(data)
@@ -38,9 +53,7 @@ function new_client(client, req) {
     client.terminate()
   })
 
-  client.on('message', (msg, _isBinary) => {
-    target.write(msg)
-  })
+  client.on('message', (msg, _isBinary) => target.write(msg))
   client.on('close', (code, reason) => {
     log(`WebSocket client disconnected: ${code} [${reason}]`)
     target.end()
@@ -49,36 +62,4 @@ function new_client(client, req) {
     log(`WebSocket client error: ${a}`)
     target.end()
   })
-}
-
-// parse source and target arguments into parts
-let source_port
-let target_host
-let target_port
-try {
-  const source_arg = argv._[0].toString()
-  const target_arg = argv._[1].toString()
-
-  source_port = Number.parseInt(source_arg, 10)
-
-  const idx = target_arg.indexOf(':')
-  if (idx < 0) {
-    throw 'target must be host:port'
-  }
-  target_host = target_arg.slice(0, idx)
-  target_port = Number.parseInt(target_arg.slice(idx + 1), 10)
-
-  if (Number.isNaN(source_port) || Number.isNaN(target_port)) {
-    throw 'illegal port'
-  }
-} catch (e) {
-  console.error('websockify.js source_port target_addr:target_port')
-  process.exit(2)
-}
-
-console.log(
-  `Proxying ws://localhost:${source_port}/ to tcp://${target_host}:${target_port}`,
-)
-
-const wsServer = new WebSocketServer({ port: source_port })
-wsServer.on('connection', new_client)
+})
