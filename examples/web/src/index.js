d39f2805fe65beca702018cc3105263b807e21a8
Hyeon Kim <simnalamburt@gmail.com>
2025-03-07 21:54:24 +0900

    examples/web: Remove browser-stdout dependency

--- examples/web/src/index.js
+++ examples/web/src/index.js
@@ -1,17 +1,34 @@
-import BrowserStdout from 'browser-stdout'
+import { Writable } from 'node:stream'
+
 import irc from 'slate-irc'
 import websocket from 'websocket-stream'
 
-const stream = new websocket('ws://localhost', 'binary')
-stream.on('error', (error) => {
+document.head.insertAdjacentHTML(
+  'beforeend',
+  `<style>
+    #root {
+      white-space: pre;
+      font-family: monospace;
+    }
+  </style>`,
+)
+
+const root = document.getElementById('root')
+
+const browser = new Writable({
+  write(chunk, _encoding, callback) {
+    root.textContent += chunk.toString()
+    callback()
+  },
+})
+
+const ws = new websocket('ws://localhost', 'binary')
+ws.on('error', (error) => {
   throw error
 })
 
-const client = irc(stream)
-client.use((irc) => irc.stream.pipe(BrowserStdout())) // Pipe to browser log.
+const client = irc(ws)
+client.use((irc) => irc.stream.pipe(browser))
 client.pass('pass')
 client.nick(`slate-${(Math.random() * 100000) | 0}`)
 client.user('tobi', 'Tobi Ferret')
-
-const root = document.getElementById('root')
-root.textContent = 'Open your console and see the irc log!'
