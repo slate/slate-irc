6c8fb2b34b798d68c8dc553a3b3a62c5275da5e2
Jihyeon Kim <simnalamburt@gmail.com>
2026-02-16 22:39:07 +0900

    Replace microbundle with tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, type UserConfig } from "tsdown";
+
+const config: UserConfig = defineConfig({
+  entry: "index.js",
+  minify: true,
+  sourcemap: true,
+  inlineOnly: ["irc-replies"],
+});
+
+export default config;
