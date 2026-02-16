6ccb7c35d588b30140bf57f1210a778fd1ad949e
Jihyeon Kim <simnalamburt@gmail.com>
2026-02-16 23:19:44 +0900

    Export index.d.mts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, type UserConfig } from "tsdown";
 
 const config: UserConfig = defineConfig({
-  entry: "src/index.js",
   minify: true,
   sourcemap: true,
   inlineOnly: ["irc-replies"],
